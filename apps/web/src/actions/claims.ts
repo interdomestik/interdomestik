091fbf6611ea139ff9477bcdf2f33c4a0da42d2e
arbenl <arben.lila@gmail.com>
2026-03-14 08:27:44 +0100

    fix: satisfy sonar re-export rule

--- apps/web/src/actions/claims.ts
+++ apps/web/src/actions/claims.ts
@@ -1,4 +1,2 @@
 'use server';
-import { cancelClaim, createClaim, updateClaimStatus, updateDraftClaim } from './claims.core';
-
-export { cancelClaim, createClaim, updateClaimStatus, updateDraftClaim };
+export { cancelClaim, createClaim, updateClaimStatus, updateDraftClaim } from './claims.core';
